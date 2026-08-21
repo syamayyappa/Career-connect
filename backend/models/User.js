069bf977e8b714611b70bd8b3c676552d30998af
syamayyappa <ayyappa.ponnada@sasi.ac.in>
2026-08-21 23:51:07 +0530

    fix: resolve 'next is not a function' Mongoose pre-save hook callback exception

--- backend/models/User.js
+++ backend/models/User.js
@@ -73,17 +73,12 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving to database
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
